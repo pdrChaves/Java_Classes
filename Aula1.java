b70e50b057b495bd7b36680280ec9ae7a8d8a858
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-02-20 08:19:55 -0300

    Update Aula1.java

--- Aula1.java
+++ Aula1.java
@@ -1,13 +1,20 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String args []){
         String mensagem = "Hello, world!!";
         System.out.println(mensagem);
         System.out.println("Hello, World!");
+        
+        
         int numero = 1234;
         System.out.println(numero);
         System.out.println(mensagem + " Bom dia!!");
         System.out.println("Bom dia");
-        mensagem = mensagem + 1234;     //funciona como conversão para string
-        mensagem = mensagem + "1234";   //funciona como string nativamente
+        
+        
+        mensagem = mensagem + 1234;         //funciona como conversão para string
+        mensagem = mensagem + "1234";       //funciona como string
+        
+        int idade = 20;
+        System.out.format("A idade de Fulano é %i", idade);     //println nao funciona com int
     }
 }
