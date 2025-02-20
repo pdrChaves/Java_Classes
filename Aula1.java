c6befc8f00062cd658217600fc49124df4513da8
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-02-20 07:49:40 -0300

    Create Aula1.java

---
+++ Aula1.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        String mensagem = "Hello, world!!!!";
+        System.out.println(mensagem);
+        System.out.println("Hello, World!");
+        int numero = 1234;
+        System.out.println(numero);
+    }
+}
