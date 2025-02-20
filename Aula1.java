199079ee058693c468bf48b6347d48b3f11b1afd
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-02-20 08:09:57 -0300

    Update Aula1.java

--- Aula1.java
+++ Aula1.java
@@ -1,9 +1,13 @@
 public class App {
     public static void main(String[] args) {
-        String mensagem = "Hello, world!!!!";
+        String mensagem = "Hello, world!!";
         System.out.println(mensagem);
         System.out.println("Hello, World!");
         int numero = 1234;
         System.out.println(numero);
+        System.out.println(mensagem + " Bom dia!!");
+        System.out.println("Bom dia");
+        mensagem = mensagem + 1234;     //funciona como conversão para string
+        mensagem = mensagem + "1234";   //funciona como string nativamente
     }
 }
