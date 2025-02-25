d0fb858a83627db01e1d00078c796d8ab469d78a
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-02-25 08:27:59 -0300

    Update Aula2.java

--- Aula2.java
+++ Aula2.java
@@ -1,3 +1,6 @@
+
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) throws Exception {
         String nome = "Fulano";
@@ -8,6 +11,7 @@ public class App {
         
         //variavel ponto flutuante
         //0.12345678910 x 10^4
+        
         // double armazena o dobro de bits na memoria (consequentemnte o dobro de bites)
         // float = 32 bits, double = 64 bits
         // float renda;     erro por numero ser muito grande
@@ -16,11 +20,18 @@ public class App {
         double renda = 0;
         System.out.println("valor inicial da renda: " +renda);
         renda= 1235.6789;
+
         System.out.println("Agora, a variavel vale " +renda);
         System.out.printf("Agora, a variavel vale %.2f %n",renda);
         System.out.printf("Agora, a variavel vale %.4f %n",renda);
         System.out.printf("Agora, a variavel vale %.6f %n",renda);
-        
+
+         // agora o formato padrao foi configurado para Estados Unidos
+        Locale.setDefault(Locale.US);
+        System.out.println("Formato americano:");
+        System.out.printf("Agora, a variavel vale %.2f %n",renda);
+        System.out.printf("Agora, a variavel vale %.4f %n",renda);
+        System.out.printf("Agora, a variavel vale %.6f %n",renda);
 
     }
 }
