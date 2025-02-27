ce3ebcccc0f40ebe55e85d4e1e7bccf997438f81
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-02-27 08:17:13 -0300

    Create Aula03.PT2.java

---
+++ Aula03.PT2.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args)
+    {
+        double x,y;
+        x = 30./7;
+        y = 30/7;
+        //Se feito com numeros inteiros, o resultado sempre sera inteiro.
+        //se feiot com numeros flutuantes, o resultado sera com numeros flutuantes.
+        System.out.println("O resultado com numeros flutuantes é: " +x);
+        System.out.println("O resultado com numeros inteiros é: " +y);
+
+
+    }
+}
