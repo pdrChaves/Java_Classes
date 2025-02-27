f8b0ac3d9260c3688ea7df76b1793efa1939f043
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-02-27 07:49:57 -0300

    Update Aula03.java

--- Aula03.java
+++ Aula03.java
@@ -4,10 +4,15 @@ public class App {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int x;
-        x = sc.nextInt();
-        System.out.println(x);
-
+        int x, y;
+        x = sc.nextInt();                    //pegará sempre o proximo inteiro 
+        y = sc.nextInt();
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+        
+        String s;
+        s = sc.next ();                      //pegará a proxima entrada que encaixe como String
+        System.out.println("s = "+s);   
 
     }
 }
