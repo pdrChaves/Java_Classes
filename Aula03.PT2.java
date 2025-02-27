ad47a452f0cfb52fc51352b15d097819f1c322b3
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-02-27 08:22:21 -0300

    Update Aula03.PT2.java

--- Aula03.PT2.java
+++ Aula03.PT2.java
@@ -8,7 +8,12 @@ public class App {
         //se feiot com numeros flutuantes, o resultado sera com numeros flutuantes.
         System.out.println("O resultado com numeros flutuantes é: " +x);
         System.out.println("O resultado com numeros inteiros é: " +y);
-
-
+        
+        double z;
+        int a = 30, b= 7;
+        z= (double)a/b;             //(double) esta tornando a de int para double, somente o a
+        System.out.println("O resultado de double z: " +z);
+        
+        
     }
 }
