51ac33ff4d0c303d1de7cae9c5e8a36e518afb67
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-02-27 08:41:21 -0300

    Update Aula03.PT2.java

--- Aula03.PT2.java
+++ Aula03.PT2.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args)
     {
-        double x,y;
+        double x,y;                 //beecrowd google, entrar como aluno.
         x = 30./7;
         y = 30/7;
         //Se feito com numeros inteiros, o resultado sempre sera inteiro.
@@ -13,7 +13,12 @@ public class App {
         int a = 30, b= 7;
         z= (double)a/b;             //(double) esta tornando a de int para double, somente o a
         System.out.println("O resultado de double z: " +z);
+        System.out.println("a raiz quadrada de " +x+ "é " +Math.sqrt(x));
+        System.out.printf("a raiz quadrada de %d é %.2f",x,Math.sqrt(x));
         
+        System.out.println(Math.sqrt(x));
+
+
         
     }
 }
